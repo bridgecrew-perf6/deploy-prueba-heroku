81f7b7f8d4018c0f57b2055c36f7642a0f89f71f
lucasrusman <rusmanlucas@gmail.com>
2022-05-02 09:43:43 -0300

    make it better

--- server.js
+++ server.js
@@ -10,6 +10,6 @@ app.use(express.static('./dist/desploy-test'));
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/deploy-test/'}),
 );
-
+  
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
